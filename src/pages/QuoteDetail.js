10b25d15686c0cb898a8d57186f69e69cd81e15f
jason7531 <54910136+jason7531@users.noreply.github.com>
2021-06-22 13:23:22 +0530

    Added detail page for quotes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,23 @@
 import { useParams, Route } from "react-router-dom";
 import React from "react";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_DATA = [
+  { id: "q1", author: "Jason", text: "Learning React" },
+  { id: "q2", author: "Max", text: "Learning Angular" },
+  { id: "q3", author: "Raze", text: "Learning Vue" },
+];
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId);
+  if (!quote) {
+    return <p>No Quote Found</p>;
+  }
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
